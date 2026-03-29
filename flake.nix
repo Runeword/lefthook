a705024f226eeadf1fc373d88ac0d4393fdc098c
Runeword <60324746+Runeword@users.noreply.github.com>
2026-03-29 18:09:18 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
-    { nixpkgs, ... }:
+    { self, nixpkgs, ... }:
     let
       systems = [
         "x86_64-linux"
@@ -10,20 +10,27 @@
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      forAllSystems = nixpkgs.lib.genAttrs systems;
     in
     {
-      devShells = forAllSystems (
+      devShells = nixpkgs.lib.genAttrs systems (
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          mkModule = path: import path { inherit pkgs self; };
+          modules = {
+            auto-msg = mkModule ./modules/auto-msg.nix;
+            format-nix = mkModule ./modules/format-nix.nix;
+            format-shell = mkModule ./modules/format-shell.nix;
+            format-toml = mkModule ./modules/format-toml.nix;
+            format-yaml = mkModule ./modules/format-yaml.nix;
+            lint-shell = mkModule ./modules/lint-shell.nix;
+          };
         in
-        {
+        modules
+        // {
           default = pkgs.mkShell {
-            buildInputs = [
-              pkgs.yamlfmt
-              pkgs.nixfmt-rfc-style
-            ];
+            inputsFrom = builtins.attrValues modules;
+            buildInputs = [ pkgs.lefthook ];
             shellHook = ''
               lefthook install
             '';
