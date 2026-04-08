12cebefa4aedc9365392cc078d3f164ef90f6a5b
Runeword <60324746+Runeword@users.noreply.github.com>
2026-04-08 21:03:21 +0200

    Update modules/format-shell.nix

--- modules/format-shell.nix
+++ modules/format-shell.nix
@@ -4,6 +4,7 @@
     buildInputs = [
       pkgs.shfmt
       pkgs.shellharden
+      (pkgs.writeShellScriptBin "format-shell" (builtins.readFile "${self}/scripts/format-shell.sh"))
     ];
     configFiles = [ "${self}/precommit-format-shell.yml" ];
   };
