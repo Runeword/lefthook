0af91ce531e929d743383bc547b171637de87d84
Runeword <60324746+Runeword@users.noreply.github.com>
2026-04-08 21:03:21 +0200

    Update modules/format-toml.nix

--- modules/format-toml.nix
+++ modules/format-toml.nix
@@ -1,7 +1,10 @@
 { self, pkgs, ... }:
 {
   config = {
-    buildInputs = [ pkgs.taplo ];
+    buildInputs = [
+      pkgs.taplo
+      (pkgs.writeShellScriptBin "format-toml" (builtins.readFile "${self}/scripts/format-toml.sh"))
+    ];
     configFiles = [ "${self}/precommit-format-toml.yml" ];
   };
 }
