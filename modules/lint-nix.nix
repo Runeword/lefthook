739f6f771a6f39e06bf9bf8708810f271af60a5e
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-15 16:46:53 +0200

    Update modules/lint-nix.nix

--- modules/lint-nix.nix
+++ modules/lint-nix.nix
@@ -4,6 +4,7 @@
     buildInputs = [
       pkgs.deadnix
       pkgs.statix
+      (pkgs.writeShellScriptBin "lint-nix" (builtins.readFile "${self}/scripts/lint-nix.sh"))
     ];
     configFiles = [ "${self}/precommit-lint-nix.yml" ];
   };
