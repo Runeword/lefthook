0319f6f669ad5b79dc7ffdccb33f66fd2bf6a1f5
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-15 16:46:53 +0200

    Update modules/lint-go.nix

--- modules/lint-go.nix
+++ modules/lint-go.nix
@@ -1,7 +1,10 @@
 { self, pkgs, ... }:
 {
   config = {
-    buildInputs = [ pkgs.golangci-lint ];
+    buildInputs = [
+      pkgs.golangci-lint
+      (pkgs.writeShellScriptBin "lint-go" (builtins.readFile "${self}/scripts/lint-go.sh"))
+    ];
     configFiles = [ "${self}/precommit-lint-go.yml" ];
   };
 }
