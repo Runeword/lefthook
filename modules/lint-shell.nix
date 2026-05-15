13d1d1b75fdfda0c5aa994dce27b19032cd7da76
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-15 16:46:53 +0200

    Update modules/lint-shell.nix

--- modules/lint-shell.nix
+++ modules/lint-shell.nix
@@ -1,7 +1,10 @@
 { self, pkgs, ... }:
 {
   config = {
-    buildInputs = [ pkgs.shellcheck ];
+    buildInputs = [
+      pkgs.shellcheck
+      (pkgs.writeShellScriptBin "lint-shell" (builtins.readFile "${self}/scripts/lint-shell.sh"))
+    ];
     configFiles = [ "${self}/precommit-lint-shell.yml" ];
   };
 }
