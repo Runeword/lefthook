14b207b2255ed5df4fc00e87fb84267bea7a530b
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-15 16:46:53 +0200

    Update modules/auto-msg.nix

--- modules/auto-msg.nix
+++ modules/auto-msg.nix
@@ -1,4 +1,9 @@
-{ self, ... }:
+{ self, pkgs, ... }:
 {
-  config.configFiles = [ "${self}/precommit-auto-msg.yml" ];
+  config = {
+    buildInputs = [
+      (pkgs.writeShellScriptBin "auto-commit" (builtins.readFile "${self}/scripts/auto-commit.sh"))
+    ];
+    configFiles = [ "${self}/precommit-auto-msg.yml" ];
+  };
 }
