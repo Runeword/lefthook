130628a44fe8c334e12b0e0c0a4d4f457d252e29
Runeword <60324746+Runeword@users.noreply.github.com>
2026-05-17 19:01:53 +0200

    Delete modules/auto-msg.nix

--- modules/auto-msg.nix
+++
@@ -1,9 +0,0 @@
-{ self, pkgs, ... }:
-{
-  config = {
-    buildInputs = [
-      (pkgs.writeShellScriptBin "auto-commit" (builtins.readFile "${self}/scripts/auto-commit.sh"))
-    ];
-    configFiles = [ "${self}/precommit-auto-msg.yml" ];
-  };
-}
